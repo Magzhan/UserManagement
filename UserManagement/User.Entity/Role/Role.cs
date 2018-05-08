1840c5ec7b983fda8f06bbb5278685f1046912c6
kazprogrammist <magzhan93@gmail.com>
2018-05-08 22:32:37 +0600

    Supporter projects have been added

---
+++ UserManagement/User.Entity/Role/Role.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace User.Entity.Role {
+    public class Role {
+        [Key]
+        public int Id { get; set; }
+
+        public string NormalizedName { get; set; }
+
+        public string NameKz { get; set; }
+
+        public string NameRu { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string DescriptionKz { get; set; }
+
+        public string DescriptionRu { get; set; }
+
+        public string DescriptionEn { get; set; }
+    }
+}
---
+++ UserManagement/User.Entity/User/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace User.Entity.User {
+    public class User {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(12)]
+        public string Xin { get; set; }
+
+        
+    }
+}
